f7291e4aee1e9e783aca02a9a4faf723b1911543
krolew <iwanekmaksymilian@gmail.com>
2026-01-22 00:33:56 +0100

    chore(deps): add nelmio/cors-bundle for CORS support
    Enables cross-origin requests from Vue.js frontend

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
